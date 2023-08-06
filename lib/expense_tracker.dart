cf5ef45b988d03f546c81e73187171dd550db41b
Rohit Pradeep Sudarsanan <rohit.psudarsanan@gmail.com>
2023-08-06 19:45:04 +0530

    expense data
    an expense data was created

--- lib/expense_tracker.dart
+++ lib/expense_tracker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -8,8 +9,37 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _expenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 10.99,
+      date: DateTime.now(),
+      category: Category.work, 
+    ),
+    Expense(
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+      category: Category.other, 
+    ),
+    Expense(
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+      category: Category.other, 
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expenses'),
+      ),
+      body: Column(
+        children:[
+          
+        ]
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expense_tracker.dart';
 
 
 
 void main() {
   runApp(
     const MaterialApp(
-      home: ...,
+      home: Expenses(),
     ),
   );
 }
